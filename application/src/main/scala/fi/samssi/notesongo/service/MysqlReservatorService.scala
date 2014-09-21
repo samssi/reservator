c148bd32e8f59c497ae0a28f625d6ada25cfb71d
Sämir Hamadi <samir.hamadi@elisa.fi>
2014-09-21 22:39:39 +0300

    Login functionality

--- application/src/main/scala/fi/samssi/notesongo/service/MysqlReservatorService.scala
+++ application/src/main/scala/fi/samssi/notesongo/service/MysqlReservatorService.scala
@@ -1,8 +1,11 @@
 package fi.samssi.notesongo.service
 
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 import fi.samssi.notesongo.service.samssi.oshopper.repository.CentralRepository
 import fi.samssi.notesongo.Logging
 import com.mongodb.casbah.commons.MongoDBObject
+import fi.samssi.notesongo.domain.User
 
 class UserRepository extends CentralRepository with Logging {
     def repositoryCollection = "users"
@@ -10,8 +13,7 @@ class UserRepository extends CentralRepository with Logging {
     def getUser(username: String) = {
       val userSearchingObject = MongoDBObject("username" -> username)
       val result = fetchCollectionFromDb(repositoryCollection).findOne(userSearchingObject)
-
-      println(withFlattenedId(result.toString))
+      parse(withFlattenedId(result.toString)).extract[User]
     }
   }
 
--- application/src/main/scala/fi/samssi/notesongo/servlet/UserServlet.scala
+++ application/src/main/scala/fi/samssi/notesongo/servlet/UserServlet.scala
@@ -1,20 +1,25 @@
 package fi.samssi.notesongo.servlet
 
-import org.scalatra.{CorsSupport, Ok}
+import org.scalatra.{Unauthorized, CorsSupport, Ok}
 import com.google.inject.Inject
 import fi.samssi.notesongo.service.UserRepository
 import fi.samssi.notesongo.domain.{User, Result}
 import fi.samssi.notesongo.token.AccessTokens
 
 class UserServlet @Inject()(userRepository: UserRepository) extends MasterServlet {
-  get("/user/:userId") {
-    userRepository.getUser("user@user.com")
+  get("/user") {
     Ok("success")
   }
 
-  put("/user/login") {
+  post("/user/login") {
     val user = parsedBody.extract[User]
-    AccessTokens.createToken(user.username)
-    Ok(Result("Success"))
+    val expected = userRepository.getUser("user@user.com")
+    if (user.equals(expected)) {
+      AccessTokens.createToken(user.username)
+      Ok(Result("Success"))
+    }
+    else {
+      Unauthorized(Result("Not a valid username or password"))
+    }
   }
 }
