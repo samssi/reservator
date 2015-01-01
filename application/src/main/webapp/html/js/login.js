69be86ba3309105c822ecd3d378d0068289a974a
Sämir Hamadi <samir.hamadi@elisa.fi>
2015-01-01 23:21:46 +0200

    Login working

--- application/src/main/webapp/html/js/login.js
+++ application/src/main/webapp/html/js/login.js
@@ -1,10 +1,11 @@
 var login = angular.module('login', ['ngCookies']);
 
-login.controller('loginController', ['$scope', '$cookieStore', '$http', function($scope, $cookieStore, $http) {
+login.controller('loginController', ['$scope', '$cookieStore', '$http', '$window', function($scope, $cookieStore, $http, $window) {
     $scope.login = function () {
-        $http.post('http://localhost:8080/reservator/v1/user/login', $scope.user).success(function(data) {
-            console.log(data.token);
-            $cookieStore.put('authorization-token', data.token);
+        $http.post('http://localhost:8080/reservator/v1/user/login', $scope.user).success(function(response) {
+            $cookieStore.put('authorization-token', response.token);
+            $cookieStore.put('username', $scope.user.username);
+            $window.location.replace('http://localhost:8080/html/reservator.html');
         });
     }
 }]);
--- application/src/main/webapp/html/js/reservator.js
+++ application/src/main/webapp/html/js/reservator.js
@@ -1,11 +1,15 @@
 var reservator = angular.module('reservator', ['ngCookies']);
 
-reservator.controller('reservationsController', ['$scope', '$cookieStore', '$http', function($scope, $cookieStore, $http) {
+reservator.controller('reservationsController', ['$scope', '$cookieStore', '$http', '$window', function($scope, $cookieStore, $http, $window) {
+    var username = $cookieStore.get('username');
     var token = $cookieStore.get('authorization-token');
-    console.log(token);
-    $http.get('http://localhost:8080/reservator/v1/reservations/samssi/' + token).success(function (data){
+    $http.get('http://localhost:8080/reservator/v1/reservations/' + username + '/' + token).success(function(data){
         console.log('response: ' + data);
-    }).error(function () {
-        console.log('Not logged in!')
+    }).error(function() {
+        $window.location.replace('http://localhost:8080/html/index.html')
     });
+}]);
+
+reservator.controller('userController', ['$scope', '$cookieStore', function($scope, $cookieStore) {
+    $scope.username = $cookieStore.get('username');
 }]);
